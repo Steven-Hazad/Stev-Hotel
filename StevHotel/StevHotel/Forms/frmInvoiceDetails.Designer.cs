6ec82635f6cfc5b2b5f639ff8a5f96d3d2b51711
Steven-Hazad <phanuth.hun@gmail.com>
2026-01-17 22:09:03 +0700

    we do the same as frminvoice just make the UI of invoice detail form

--- StevHotel/StevHotel/Forms/frmInvoiceDetails.Designer.cs
+++ StevHotel/StevHotel/Forms/frmInvoiceDetails.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class frmInvoiceDetails
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,20 +13,154 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblGuest = new System.Windows.Forms.Label();
+            this.lblRoom = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.dgvInvoiceItems = new System.Windows.Forms.DataGridView();
+            this.btnAddExtra = new System.Windows.Forms.Button();
+            this.dgvPayments = new System.Windows.Forms.DataGridView();
+            this.btnAddPayment = new System.Windows.Forms.Button();
+            this.btnMarkPaid = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInvoiceItems)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPayments)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblGuest
+            // 
+            this.lblGuest.AutoSize = true;
+            this.lblGuest.Location = new System.Drawing.Point(30, 30);
+            this.lblGuest.Name = "lblGuest";
+            this.lblGuest.Size = new System.Drawing.Size(0, 20);
+            this.lblGuest.TabIndex = 0;
+            // 
+            // lblRoom
+            // 
+            this.lblRoom.AutoSize = true;
+            this.lblRoom.Location = new System.Drawing.Point(30, 60);
+            this.lblRoom.Name = "lblRoom";
+            this.lblRoom.Size = new System.Drawing.Size(0, 20);
+            this.lblRoom.TabIndex = 1;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(30, 90);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(0, 20);
+            this.lblTotal.TabIndex = 2;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(30, 120);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 20);
+            this.lblStatus.TabIndex = 3;
+            // 
+            // dgvInvoiceItems
+            // 
+            this.dgvInvoiceItems.AllowUserToAddRows = false;
+            this.dgvInvoiceItems.AllowUserToDeleteRows = false;
+            this.dgvInvoiceItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInvoiceItems.Location = new System.Drawing.Point(30, 160);
+            this.dgvInvoiceItems.Name = "dgvInvoiceItems";
+            this.dgvInvoiceItems.ReadOnly = true;
+            this.dgvInvoiceItems.RowHeadersWidth = 51;
+            this.dgvInvoiceItems.RowTemplate.Height = 29;
+            this.dgvInvoiceItems.Size = new System.Drawing.Size(400, 150);
+            this.dgvInvoiceItems.TabIndex = 4;
+            // 
+            // btnAddExtra
+            // 
+            this.btnAddExtra.Location = new System.Drawing.Point(30, 320);
+            this.btnAddExtra.Name = "btnAddExtra";
+            this.btnAddExtra.Size = new System.Drawing.Size(120, 35);
+            this.btnAddExtra.TabIndex = 5;
+            this.btnAddExtra.Text = "Add Extra";
+            this.btnAddExtra.UseVisualStyleBackColor = true;
+            this.btnAddExtra.Click += new System.EventHandler(this.btnAddExtra_Click);
+            // 
+            // dgvPayments
+            // 
+            this.dgvPayments.AllowUserToAddRows = false;
+            this.dgvPayments.AllowUserToDeleteRows = false;
+            this.dgvPayments.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPayments.Location = new System.Drawing.Point(450, 160);
+            this.dgvPayments.Name = "dgvPayments";
+            this.dgvPayments.ReadOnly = true;
+            this.dgvPayments.RowHeadersWidth = 51;
+            this.dgvPayments.RowTemplate.Height = 29;
+            this.dgvPayments.Size = new System.Drawing.Size(300, 150);
+            this.dgvPayments.TabIndex = 6;
+            // 
+            // btnAddPayment
+            // 
+            this.btnAddPayment.Location = new System.Drawing.Point(450, 320);
+            this.btnAddPayment.Name = "btnAddPayment";
+            this.btnAddPayment.Size = new System.Drawing.Size(120, 35);
+            this.btnAddPayment.TabIndex = 7;
+            this.btnAddPayment.Text = "Add Payment";
+            this.btnAddPayment.UseVisualStyleBackColor = true;
+            this.btnAddPayment.Click += new System.EventHandler(this.btnAddPayment_Click);
+            // 
+            // btnMarkPaid
+            // 
+            this.btnMarkPaid.Location = new System.Drawing.Point(580, 320);
+            this.btnMarkPaid.Name = "btnMarkPaid";
+            this.btnMarkPaid.Size = new System.Drawing.Size(120, 35);
+            this.btnMarkPaid.TabIndex = 8;
+            this.btnMarkPaid.Text = "Mark as Paid";
+            this.btnMarkPaid.UseVisualStyleBackColor = true;
+            this.btnMarkPaid.Click += new System.EventHandler(this.btnMarkPaid_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(710, 320);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(120, 35);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // frmInvoiceDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "frmInvoiceDetails";
+            this.ClientSize = new System.Drawing.Size(800, 380);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnMarkPaid);
+            this.Controls.Add(this.btnAddPayment);
+            this.Controls.Add(this.btnAddExtra);
+            this.Controls.Add(this.dgvPayments);
+            this.Controls.Add(this.dgvInvoiceItems);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblRoom);
+            this.Controls.Add(this.lblGuest);
+            this.Name = "frmInvoiceDetails";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Invoice Details";
+            this.Load += new System.EventHandler(this.frmInvoiceDetails_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInvoiceItems)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPayments)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
-        #endregion
+        private System.Windows.Forms.Label lblGuest;
+        private System.Windows.Forms.Label lblRoom;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.DataGridView dgvInvoiceItems;
+        private System.Windows.Forms.Button btnAddExtra;
+        private System.Windows.Forms.DataGridView dgvPayments;
+        private System.Windows.Forms.Button btnAddPayment;
+        private System.Windows.Forms.Button btnMarkPaid;
+        private System.Windows.Forms.Button btnClose;
     }
 }
